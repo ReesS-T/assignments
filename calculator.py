531b7bd349cbefc9206663a2176e41f22c60fb63
@ReesS-T <rsweeneytaylor@hks.harvard.edu>
2020-01-12 15:43:15 -0500

    Added a function that squares intergers multiple times

--- calculator.py
+++ calculator.py
@@ -16,6 +16,22 @@ def subtract(a,b):
 def divide(a,b):
     return a/b
 
-print("I'm going use the calculator functions to multiply 5 and 6")
+# Defining a function that squares a number
+def square(a):
+    return a*a
+
+# Defining a function that cubes a number
+def cube(a):
+    return a*a*a  
+
+# Defining a function that squares a number multiple times
+def square_n_times (a,b):
+	return a**(2*b)
+
+print("I'm going use the calculator function to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+print("I'm going to use the calculator function to square 4 three times")
+y=square_n_times (4,3)
+print(y)
