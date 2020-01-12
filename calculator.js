f9cedf9f98f381a0c578bfde56087f6297cfbd56
@ReesS-T <rsweeneytaylor@hks.harvard.edu>
2020-01-12 15:53:28 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+//Making a function that adds two numbers
+function addition (a,b) {return a+b}
+
+//Making a function that subtracts two numbers
+function subraction (a,b) {return a-b}
+
+//Making a function that multiplies two numbers
+function multiplcation (a,b) {return a*b}
+
+//Making a function that divides two numbers
+function division (a,b) {return a/b}
+
+
+x=addition(3,4)
+console.log (x)
