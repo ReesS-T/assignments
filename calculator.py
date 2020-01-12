9d62646b8ffed88425624c65286242ebb0bac44a
@ReesS-T <rsweeneytaylor@hks.harvard.edu>
2020-01-12 13:42:51 -0500

    "add functions to calculator"

---
+++ calculator.py
@@ -0,0 +1,18 @@
+# Defining a function that multiplies two numbers
+def multiply(a,b):
+    return a * b
+
+
+# Defining a function that adds two numbers
+def add(a,b):
+    return a + b
+
+# Defining a function that subtracts two numbers
+def subtract(a,b):
+    return a - b
+
+
+# Defining a function that divides two numbers
+def divide(a,b):
+    return a/b
+
