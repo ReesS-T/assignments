08ef62a68664c0ca8591d437bd1b47adffdead80
@ReesS-T <rsweeneytaylor@hks.harvard.edu>
2020-01-12 15:55:02 -0500

    Added a printout of 30 divided by 6

--- calculator.js
+++ calculator.js
@@ -10,6 +10,7 @@ function multiplcation (a,b) {return a*b}
 //Making a function that divides two numbers
 function division (a,b) {return a/b}
 
-
-x=addition(3,4)
-console.log (x)
+//Printing result
+console.log("I'm going use the calculator functions to divide 30 by 6")
+var x = division(30,6)
+console.log(x)
